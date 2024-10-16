e83aa17ad0fe0815ebfda84bcccf6dad0ccd85cc
camilasanchezu <camila.sanchez.urresta@udla.edu.ec>
2024-10-15 21:00:55 -0500

    Change structure

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Login } from 'src/app/Interfaces/login';
 import { UsuarioService } from 'src/app/Services/usuario.service';
@@ -9,60 +9,55 @@ import { UtilidadService } from 'src/app/Reutilizable/utilidad.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  formularioLogin:FormGroup;
-  ocultarPassword:boolean = true;
-  mostrarLoading:boolean= false;
+  formularioLogin: FormGroup;
+  ocultarPassword: boolean = true;
+  mostrarLoading: boolean = false;
 
   constructor(
-    private fb:FormBuilder,
+    private fb: FormBuilder,
     private router: Router,
     private _usuarioServicio: UsuarioService,
     private _utilidadServicio: UtilidadService
-  ) { 
+  ) {
     this.formularioLogin = this.fb.group({
-      email:['',Validators.required],
-      password:['',Validators.required]
+      email: ['', Validators.required],
+      password: ['', Validators.required],
     });
-
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  iniciarSesion(){
-
+  iniciarSesion() {
     this.mostrarLoading = true;
 
-    const request: Login ={
-      correo : this.formularioLogin.value.email,
-      clave : this.formularioLogin.value.password
-    }
+    const request: Login = {
+      correo: this.formularioLogin.value.email,
+      clave: this.formularioLogin.value.password,
+    };
 
     this._usuarioServicio.iniciarSesion(request).subscribe({
       next: (data) => {
-        if(data.status){
+        console.log(data);
+        if (data.status) {
           this._utilidadServicio.guardarSesionUsuario(data.value);
-          this.router.navigate(["pages"])
-        }else
-          this._utilidadServicio.mostrarAlerta("No se encontraron coincidencias","Opps!")
-
+          this.router.navigate(['/pages']).catch((err) => {
+            console.error('Error al redirigir:', err);
+          });
+        } else
+          this._utilidadServicio.mostrarAlerta(
+            'No se encontraron coincidencias',
+            'Opps!'
+          );
       },
-      complete : () =>{
+      complete: () => {
         this.mostrarLoading = false;
       },
-      error : ()=>{
-        this._utilidadServicio.mostrarAlerta("Hubo un error", "Opps!")
-
-      }
-    })
-
-
+      error: () => {
+        this._utilidadServicio.mostrarAlerta('Hubo un error', 'Opps!');
+      },
+    });
   }
-
-
-
 }
--- src/app/Services/usuario.service.ts
+++ src/app/Services/usuario.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ResponseApi } from '../Interfaces/response-api';
@@ -8,31 +8,30 @@ import { Login } from '../Interfaces/login';
 import { Usuario } from '../Interfaces/usuario';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuarioService {
+  private urlApi: string = environment.endpoint + 'Usuario/';
 
-  private urlApi:string = environment.endpoint + "Usuario/";
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  iniciarSesion(request: Login):Observable<ResponseApi>{
-    return this.http.post<ResponseApi>(`${this.urlApi}IniciarSesion`,request)
+  iniciarSesion(request: Login): Observable<ResponseApi> {
+    return this.http.post<ResponseApi>(`${this.urlApi}IniciarSesion`, request);
   }
 
-  lista():Observable<ResponseApi>{
-    return this.http.get<ResponseApi>(`${this.urlApi}Lista`)
+  lista(): Observable<ResponseApi> {
+    return this.http.get<ResponseApi>(`${this.urlApi}Lista`);
   }
 
-  guardar(request: Usuario):Observable<ResponseApi>{
-    return this.http.post<ResponseApi>(`${this.urlApi}Guardar`,request)
+  guardar(request: Usuario): Observable<ResponseApi> {
+    return this.http.post<ResponseApi>(`${this.urlApi}Guardar`, request);
   }
 
-  editar(request: Usuario):Observable<ResponseApi>{
-    return this.http.put<ResponseApi>(`${this.urlApi}Editar`,request)
+  editar(request: Usuario): Observable<ResponseApi> {
+    return this.http.put<ResponseApi>(`${this.urlApi}Editar`, request);
   }
 
-  eliminar(id: number):Observable<ResponseApi>{
-    return this.http.delete<ResponseApi>(`${this.urlApi}Eliminar/${id}`)
+  eliminar(id: number): Observable<ResponseApi> {
+    return this.http.delete<ResponseApi>(`${this.urlApi}Eliminar/${id}`);
   }
 }
